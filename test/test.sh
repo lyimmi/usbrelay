a6ea81d206c1714499d40dd0a33d747e5161cfa3
lyimmi <levente.zambo@gmail.com>
2023-09-02 12:27:20 +0200

    fix test build

--- test/test.sh
+++ test/test.sh
@@ -12,13 +12,13 @@ commands=(
 "list"
 )
 tests=(
-"umockdev-run --device relay.umockdev --pcap $PCAPPATH=01-list-start.pcapng usbrelay list"
-"umockdev-run --device relay.umockdev --pcap $PCAPPATH=02-on-all.pcapng usbrelay on REL01 all"
-"umockdev-run --device relay.umockdev --pcap $PCAPPATH=03-off-all.pcapng usbrelay off REL01 all"
-"umockdev-run --device relay.umockdev --pcap $PCAPPATH=04-toggle-all.pcapng usbrelay toggle REL01 all"
-"umockdev-run --device relay.umockdev --pcap $PCAPPATH=05-setserial-rel01-rel02.pcapng usbrelay setserial REL01 REL02"
-"umockdev-run --device relay.umockdev --pcap $PCAPPATH=06-setserial-rel02-rel01.pcapng usbrelay setserial REL02 REL01"
-"umockdev-run --device relay.umockdev --pcap $PCAPPATH=07-list-end.pcapng usbrelay list"
+"umockdev-run --device relay.umockdev --pcap $PCAPPATH=01-list-start.pcapng /tmp/usbrelay list"
+"umockdev-run --device relay.umockdev --pcap $PCAPPATH=02-on-all.pcapng /tmp/usbrelay on REL01 all"
+"umockdev-run --device relay.umockdev --pcap $PCAPPATH=03-off-all.pcapng /tmp/usbrelay off REL01 all"
+"umockdev-run --device relay.umockdev --pcap $PCAPPATH=04-toggle-all.pcapng /tmp/usbrelay toggle REL01 all"
+"umockdev-run --device relay.umockdev --pcap $PCAPPATH=05-setserial-rel01-rel02.pcapng /tmp/usbrelay setserial REL01 REL02"
+"umockdev-run --device relay.umockdev --pcap $PCAPPATH=06-setserial-rel02-rel01.pcapng /tmp/usbrelay setserial REL02 REL01"
+"umockdev-run --device relay.umockdev --pcap $PCAPPATH=07-list-end.pcapng /tmp/usbrelay list"
 )
 
 for (( i = 0; i < ${#tests[@]} ; i++ )); do
